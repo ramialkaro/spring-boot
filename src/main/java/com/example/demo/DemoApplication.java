8e9524fa8bdaa8a04efea643dc61841d4d96c6a7
rami <rami.al-karo@holda.fi>
2020-09-02 13:40:31 +0300

    link, vote, comment models added to project

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static  final Logger log = (Logger) LoggerFactory.getLogger(SpringApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner runner (){
+		return args ->  {
+			log.error("Error");
+			log.warn("warn");
+			log.info("info");
+			log.debug("debug");
+			log.trace("trace");
+		};
+	}
+
 
 }
---
+++ src/main/java/com/example/demo/domain/Vote.java
@@ -0,0 +1,43 @@
+package com.example.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+
+    // user
+    // link
+
+    public Vote(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "id=" + id +
+                ", vote=" + vote +
+                '}';
+    }
+}
