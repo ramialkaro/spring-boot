e186122ae8301898fc0287d7fec9c07315f241f5
rami <rami.al-karo@holda.fi>
2020-09-02 15:17:02 +0300

    link, comment repositories added

---
+++ src/main/java/com/example/demo/repository/LinkRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+
+}
